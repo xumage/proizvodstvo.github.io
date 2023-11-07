9a784aaeaaab14735ac8bca0101adc4b577cb378
Maris <89835500+xumage@users.noreply.github.com>
2023-11-07 18:02:51 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// СЛАЙДЕР
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  keyboard: true,
+  slidesPerView: 4,
+});
+
+// СЛАЙДЕР
+
+// ПОП АП
+
+// const openPopUP = document.getElementById("open-popup");
+// const closePopUp = document.getElementById("popup-close");
+// const popUp = document.getElementById("popup");
+//
+// openPopUP.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   popUp.classList.add("active");
+// });
+//
+// closePopUp.addEventListener("click", () => {
+//   popUp.classList.remove("active");
+// });
+
+// ПОП АП
